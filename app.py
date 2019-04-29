8d3f2dbbdab70de72cd29d41a35df04ebf67a6e0
Bruno <fernando23296@gmail.com>
2019-04-28 23:06:10 -0700

    Commit

--- app.py
+++ app.py
@@ -41,5 +41,13 @@ def verify_fb_token(token_sent):
     return 'Invalid verification token'
 
 
+
+def get_message():
+    sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
+    return random.choice(sample_responses)
+
+
+
+
 if __name__ == "__main__":
     app.run()
